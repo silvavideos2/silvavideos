05d81b2797a84662d265b8193787932a70f50892
silva <vitor0119n@gmail.com>
2021-08-04 10:34:47 -0300

    First commit

--- index.js
+++ index.js
@@ -145,8 +145,8 @@ async function starts() {
 					group: '❌ SO PODE SER USADO EM GRUPO MEU CHAPA❌',
 					ownerG: 'ISSO E COMANDO PARA O ADEMIR KKK❌',
 					ownerB: '❌ ESSE COMANDO SO PODE SER USADO PELO SILVA❌',
-					admin: 'ESSE COMANDO SO PODE SER USADO PELO SILVA KKK❌',
-					Badmin: 'ME COLOCA DE ADM AI KKK',
+					admin: 'ESSE COMANDO SO PODE SER USADO PELO SILVA ❌',
+					Badmin: 'ME COLOCA DE ADM AI pow',
 				}
 			}
 
@@ -342,7 +342,7 @@ client.sendMessage(from, imageh, image, {thumbnail:fs.readFileSync('./fotobot/fo
 					if (!isOwner) return reply(mess.only.ownerB)
 					const btrai = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": btre, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('fotobot/foto.jpeg')} } }
                     prefix = args[0]
-					reply(`SINAL ERRADO KKKK: ${prefix}`)
+					reply(`SINAL ERRADO BURRAO MANE: ${prefix}`)
 					break
 				/*case 'loli':
 					loli.getSFWLoli(async (err, res) => {
